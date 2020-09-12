65d10160dccd992adc087c1217ce2b692869ff2b
hellokirilee <hello.kiri.lee@gmail.com>
2020-09-12 11:16:31 +0800

    created cool todos with cat emojis

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import TodoItem from "./components/TodoItem";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <h1>My todo list ✔</h1>
+    <TodoItem todo="Ride a dino 🐱‍🐉"/>
+    <TodoItem todo="Do Some hacking 🐱‍💻"/>
+    <TodoItem todo="Some cool stunts🐱‍🏍"/>
+
+  </div>
   );
 }
 
