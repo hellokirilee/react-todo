5d5b9548576d3b7d6aa759e1712233928065056a
hellokirilee <hello.kiri.lee@gmail.com>
2020-09-12 12:28:14 +0800

    can now complete items

--- src/App.js
+++ src/App.js
@@ -6,25 +6,37 @@ import "./App.css";
 function App() {
   //variables
   const [todos, setTodos] = useState([
-    "Ride a dino 🐱‍🐉",
-    "Do Some hacking 🐱‍💻",
-    "Some cool stunts🐱‍🏍",
+    { text: "Ride a dino 🐱‍🐉", isCompleted: false },
+    { text: "Do Some hacking 🐱‍💻", isCompleted: false},
+    { text: "Some cool stunts🐱‍🏍", isCompleted: false},
   ]);
+
   //methods - secret? alternative? something like that function - can google
   // this updates state to include above, plus newly added todo item
   const addTodo = (text) => {
-    const newTodos = [...todos, text];
+    const newTodos = [...todos, { text }];
     setTodos(newTodos);
   };
-
-  // index creates identifier - useful in the future
+// index is used here so it knows which item it's making the change to
+  const completeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+  
+  // index 
+  //creates identifier - useful in the future
   return (
   <div className = "app">
     <div className = "todo-list">
     
     <h1>My todo list ✔</h1>
     {todos.map((todo, index) => (
-      <TodoItem todo={todo} key={index} />
+      <TodoItem 
+      todo={todo} 
+      key={index}
+      index={index}
+      completeTodo={completeTodo}/>
     ))}
 
     <TodoForm addTodo={addTodo} />
--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -2,8 +2,16 @@ import React from "react";
 import "./TodoItem.css";
 
 function TodoItem(props) {
-    const { todo } = props;
-    return <div>{todo}</div>;
+    const { todo, index, completeTodo } = props;
+    return (
+    <div className={`todo ${todo.isCompleted ? "complete" : ""}`}>
+        {todo.text}
+        <div>
+            <button onClick={() => completeTodo(index)}>Complete</button>
+
+        </div>
+        </div>
+        );
 }
 
 export default TodoItem;
